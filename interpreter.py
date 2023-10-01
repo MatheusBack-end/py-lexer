8f4589502279a50f7d65b4c0270b1120ae9a2575
MatheusBack-end <email.matheussilva2@gmail.com>
2023-10-01 14:14:35 -0300

    nodes, nodes and more nodes.. =]

--- interpreter.py
+++ interpreter.py
@@ -1,8 +1,11 @@
-class Node():
+from node import Node
+
+class Nodev():
 
     nodes = []
     value = None
     key   = None
+    previous_scope = None
 
     def __init__(self, key, value):
         self.key = key
@@ -15,43 +18,69 @@ class Interpreter():
     pos = 0
     current_token = None
     document_node = None
+    scope = None
 
     def __init__(self, tokens):
         self.tokens = tokens
         self.document_node = Node('document_node', None)
     
     def interpreter(self):
+        self.scope = self.document_node
+        self.scope.nodes = []
         self.current_token = self.tokens[self.pos]
 
         while self.eat():
             pass
-
-        for o in range(0, len(self.document_node.nodes), 1):
-            print(self.document_node.nodes[o].value)
     
     def next_token(self):
         self.pos += 1
         return self.tokens[self.pos]
 
+    def return_scope(self):
+        previous = self.scope.previous_scope
+        
+        if previous == None:
+            self.scope = self.document_node
+
+        self.scope = previous
+
     def eat(self):
         if self.current_token.type == 'eof':
             return False
+        
+        if self.current_token.value == '}':
+            self.return_scope()
 
         if self.current_token.type == 'identifier':
             key = self.current_token
 
             self.consume(['identifier'])
             self.consume(['operator'])
+            
+            if self.current_token.value == '{':
+                new_scope = Node(key.value, '(array)')
+                new_scope.nodes = []
+                
+                new_scope.previous_scope = self.scope
+                self.scope.nodes.append(new_scope)
+                self.scope = new_scope
+                self.consume(['operator'])
+                
+
+                return True
 
             value = self.current_token
             self.consume(['identifier', 'string'])
 
             node = Node(key.value, value.value)
-            self.document_node.nodes.append(node)
+            node.nodes = []
+            self.scope.nodes.append(node)
 
             return True
 
-        
+    def eval(self):
+        pass
+
     def consume(self, token_types):
         for i in range(0, len(token_types), 1):
             if self.current_token.type == token_types[i]:
