9b50be99c26e548abfe14bbfa6a8e540ae6f54f5
MatheusBack-end <email.matheussilva2@gmail.com>
2023-10-04 20:26:18 -0300

    code refactoring >:/

---
+++ fbx_to_obj.py
@@ -0,0 +1,13 @@
+class FbxToObj():
+
+    def __init__(self, fbx_node):
+        self.fbx_node = fbx_node
+        self.fbx_to_mesh()
+
+    def get_objects(self):
+        return self.fbx_node.get('Objects')
+
+    def fbx_to_mesh(self):
+        objects = self.get_objects()
+
+        print(objects.get('Model').value[0])
